559b800f302ea42532ab3a9b5e02e66fee93e4a6
Amrit Kushwaha <amritkushwaha3@gmail.com>
2022-07-23 01:53:26 +0530

    Create Merge Sorted Array.py

---
+++ DS I/Day 2/Merge Sorted Array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def merge(self, nums1, m, nums2, n):
+        i=0
+        for x in range(len(nums1)):
+            if i>=n:
+                break
+            if nums1[x]==0:
+                nums1[x]=nums2[i]
+                i+=1           
+        nums1.sort()        
